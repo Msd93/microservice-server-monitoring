0f56d6cd94aecdf9e53c82e306dff58d88f3cdb2
Mamadou Seydou Diallo <diallodiery301@gmail.com>
2021-05-30 00:31:49 +0000

    boostrap project

---
+++ src/test/java/sn/pad/monitoring/PadServerMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.pad.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadServerMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
